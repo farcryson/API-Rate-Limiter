2edb2dbc52f11cbbd0800580082a13a9351972e7
Varshith <o.varshith5454@gmail.com>
2025-12-26 02:05:31 -0500

    added Token bucket method

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ await client.connect();
 console.log("✅ Connected to Redis");
 
 
+//sliding window 
 async function rateLimiter(req, res, next) {
         const ip = req.ip;
         console.log(ip);
@@ -22,14 +23,14 @@ async function rateLimiter(req, res, next) {
         const now = Date.now();
         const windowStart = now - 60000;
 
-    const result = await client.multi()
-                    .zAdd(key, [{ value: now + '-' + Math.random(), score: now}])
-                    .zRemRangeByScore(key, 0, windowStart)
-                    .zCard(key)
-                    .expire(key, 60)
+    const result = await client.multi()     // way to execute mutiple commands at once
+                    .zAdd(key, [{ value: now + '-' + Math.random(), score: now}])   // add key, value and score
+                    .zRemRangeByScore(key, 0, windowStart)  // remove the range before 60 sec from now
+                    .zCard(key)     // count no of values in the current range
+                    .expire(key, 60)    // set the expiry for the key
                     .exec();
 
-    const requestCount = result[2];
+    const requestCount = result[2];    
     if(requestCount > 10) {
         res.status(429).send({
             "message": "Too many requests"
@@ -40,7 +41,44 @@ async function rateLimiter(req, res, next) {
     }
 };
 
-app.get('/', rateLimiter, (req, res) => {
+
+// Token Bucket method
+async function tokenBucket(req, res, next) {
+    const ip = req.ip;
+    const key = `token_bucket:${ip}`;
+    const maxTokens = 10;
+    const refillRate =  1;
+    
+    const bucket = await client.hGetAll(key);
+    const currentTokens = bucket.tokens ? parseFloat(bucket.tokens) : maxTokens;
+    let lastRefillTime = bucket.lastRefill ? parseInt(bucket.lastRefill) : Date.now(); 
+
+    const elapsedTime = (Date.now() - lastRefillTime) / 1000;
+
+    let newTokens = Math.min(10, currentTokens + (elapsedTime * refillRate));
+
+    if(newTokens >= 1) {
+        newTokens--;
+        lastRefillTime = Date.now();
+        await client.hSet(key, {
+            'tokens': newTokens,
+            'lastRefill': lastRefillTime
+        })
+        next();
+    }
+    else {
+        res.status(429).send({
+            "message": "Too many requests"
+        })
+    }
+};
+
+
+// app.get('/', rateLimiter, (req, res) => {
+//     res.send('<h1> API is working! </h1>');
+// });
+
+app.get('/', tokenBucket, (req, res) => {
     res.send('<h1> API is working! </h1>');
 });
 
